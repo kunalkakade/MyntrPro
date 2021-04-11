2c4b998de32fc4fcd9c20bafef10119ed7c786d2
kunal kakade <kunalkakade808@gmail.com>
2021-04-11 23:07:46 +0900

    fix res

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import csv
-import json
 import pandas as pd
 
 from flask import Flask
@@ -12,18 +10,17 @@ data = []
 xl_file = pd.ExcelFile('./static/myntra.xlsx')
 
 dfs = pd.read_excel(xl_file, sheet_name="Sheet0")[:50]
-# print(dfs)
-# dfs.to_dict()
 
 
 @app.route('/products')
 def list_products():
-    return dfs.to_json(orient='records', lines=True)
+    return dfs.to_json(orient='records')
 
 
 @app.route('/')
 def main():
     return "hi there"
+
 
 if __name__ == '__main__':
     app.run()
