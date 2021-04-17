4d1d385d8d36b211a7addbfa4c929c105cec9446
kunal kakade <kunalkakade808@gmail.com>
2021-04-17 17:01:46 +0900

    add category

--- app.py
+++ app.py
@@ -9,12 +9,39 @@ CORS(app)
 data = []
 xl_file = pd.ExcelFile('./static/myntra.xlsx')
 
-dfs = pd.read_excel(xl_file, sheet_name="Sheet0")[:200]
+dfs = pd.read_excel(xl_file, sheet_name="Sheet0")[:800]
 
 
 @app.route('/products')
 def list_products():
     return dfs.to_json(orient='records')
+
+
+@app.route('/products/mens')
+def list_products_mens():
+    # print(dfs)
+    df1 = dfs[dfs['brand'].str.contains("Men")]
+    return df1.to_json(orient='records')
+
+
+@app.route('/products/women')
+def list_products_women():
+    # print(dfs)
+    df1 = dfs[dfs['brand'].str.contains("Women")]
+    return df1.to_json(orient='records')
+
+
+@app.route('/products/boy')
+def list_products_boy():
+    df1 = dfs[dfs['brand'].str.contains("Boys")]
+    return df1.to_json(orient='records')
+
+
+@app.route('/products/girl')
+def list_products_girl():
+    df1 = dfs[dfs['brand'].str.contains("Girls")]
+    return df1.to_json(orient='records')
+
 
 @app.route('/')
 def main():
